121aeb15657ef24796295a2d193dbbfe97f37f35
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2023-11-05 12:50:55 +0530

    Add break statement to loops

--- main.py
+++ main.py
@@ -26,11 +26,27 @@ CLASS_NAMES = ['Tomato_Bacterial_spot',
                'Tomato__powdery_mildew',
                'Tomato_healthy']
 
+disease_mapping = {
+    "Tomato_Bacterial_spot": "bacterial_spot",
+    "Tomato_Early_blight": "early_blight",
+    "Tomato_Late_blight": "late_blight",
+    "Tomato_Leaf_Mold": "leaf_mold",
+    "Tomato_Septoria_leaf_spot": "septoria_leaf_spot",
+    "Tomato_Spider_mites_Two_spotted_spider_mite": "spider_mites",
+    "Tomato__Black_mold": "black_mold",
+    "Tomato__Gray_spot": "gray_spot",
+    "Tomato__Target_Spot": "target_spot",
+    "Tomato__Tomato_YellowLeaf__Curl_Virus": "yellow_leaf_curl",
+    "Tomato__Tomato_mosaic_virus": "mosaic_virus",
+    "Tomato__powdery_mildew": "powdery_mildew",
+    "Tomato_healthy": "healthy"
+}
+
 
 class ExtraSymptomModelOut(BaseModel):
-    hasLeafSymptom: str = 'Lesion_symptom'
-    # hasLeafSymptomColor: str = None
-    hasFruitSymptom: str = 'Spot_symptom'
+    hasLeafSymptom: str = 'lesions'
+    hasLeafSymptomColour: str = 'yellow'
+    #hasFruitSymptom: str = 'Spot_symptom'
     # hasFruitSymptomColor: str = None
     # hasStemSymptom: str = None
     # hasStemSymptomColor: str = None
@@ -44,22 +60,21 @@ class ExtraSymptomModelOut(BaseModel):
 
 def build_sparql_query(symptoms):
     # Build SPARQL query based on symptoms
-    query = """
-        SELECT ?disease
-        WHERE {
-    """
+    query = """PREFIX OntoML: <https://github.com/mtbstn24/OntoMLv3#>
+SELECT DISTINCT (strafter(STR(?disease), "#") AS ?diseaseName)
+WHERE {
+?disease rdf:type OntoML:Disease.
+"""
     for symptom, value in symptoms.items():
         if value:
-            query += f"?disease ontology_ns:{symptom} ontology_ns:{value}.\n"
+            query += f"?disease OntoML:{symptom} OntoML:{value}.\n"
     query += "}"
-
-    print(query)
     return query
 
 
 @app.get("/ping")
 async def ping():
-    return "Hello, I am aliveeee"
+    return "Hello, I am alive"
 
 
 def read_file_as_image(data, target_size=(224, 224)) -> np.ndarray:
@@ -79,21 +94,14 @@ async def predict(
     resized_image = tf.image.resize(np.array(image), (224, 224))
     img_batch = np.expand_dims(resized_image, 0)
     predictions = MODEL.predict(img_batch)
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence_threshold = 0.0
-    confidence = np.max(predictions[0])
     class_confidence_map = dict(zip(CLASS_NAMES, predictions[0]))
     filtered_diseases = [disease for disease,
                          confidence in class_confidence_map.items() if confidence >= confidence_threshold]
     Deep_Model_Sorted_Disease = sorted(
         filtered_diseases, key=lambda x: class_confidence_map[x], reverse=True)
 
-    # Deep_Model_Sorted_Disease = sorted(
-    #     CLASS_NAMES, key=lambda x: class_confidence_map[x], reverse=True)
-    # print(predictions[0])
-    print(Deep_Model_Sorted_Disease)
-
-    github_raw_uri = "https://raw.githubusercontent.com/Sribarathvajasarma/Plant_Disease_Ontology/main/Plant_Disease_ontology.owl"
+    github_raw_uri = "https://raw.githubusercontent.com/mtbstn24/OntoML/main/OntoMLv3.rdf"
 
     # Fetch the RDF file from the GitHub repository
     response = requests.get(github_raw_uri)
@@ -103,56 +111,47 @@ async def predict(
         g = Graph()
         g.parse(data=response.text, format="application/rdf+xml")
 
-        ontology_ns = Namespace(
-            "https://raw.githubusercontent.com/Sribarathvajasarma/Plant_Disease_Ontology/main/Plant_Disease_ontology.owl#")
-        rdf_ns = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-
-        # SPARQL query to extract diseases and their symptoms
-        # query = """
-        #     SELECT ?disease
-        #     WHERE {
-        #         ?disease ontology_ns:hasFruitSymptom ontology_ns:Spot_symptom.
-        #     }
-        # """
-
         query = build_sparql_query(EXTRA_SYMPTOMS)
 
         # Execute the SPARQL query
-        results = g.query(
-            query, initNs={"ontology_ns": ontology_ns, "rdf": rdf_ns})
+        results = g.query(query)
 
         ontology_satisfying_diseases = []
 
-        # Extract and print the results
         for row in results:
-            disease_uri = row.disease
-            disease_name = disease_uri.split("#")[-1]
-            ontology_satisfying_diseases.append(disease_name)
+            disease = row.diseaseName
+            ontology_satisfying_diseases.append(disease.value)
 
-        output_disease = []
-        # selection algorithm
+        print("Ontology disease")
+
+        print(ontology_satisfying_diseases)
+
+        found = False
+        output_disease = "Not found"
         if len(ontology_satisfying_diseases) == 0:
-            output_disease.append(Deep_Model_Sorted_Disease[0])
-        elif (len(ontology_satisfying_diseases) != 0 and Deep_Model_Sorted_Disease[0]):
-            output_disease = ontology_satisfying_diseases
-        elif (len(ontology_satisfying_diseases) == 0 and Deep_Model_Sorted_Disease[0] == 0):
-            return {
-                "Couldn't predict the disease using given information"
-            }
+            output_disease = Deep_Model_Sorted_Disease[0]
+        elif len(ontology_satisfying_diseases) == 1:
+            output_disease = ontology_satisfying_diseases[0]
+        elif len(ontology_satisfying_diseases) == 0 and len(Deep_Model_Sorted_Disease) == 0:
+            print("Couldn't find the disease with given information")
         else:
+
             for deep_disease in Deep_Model_Sorted_Disease:
-                for onto_disease in Deep_Model_Sorted_Disease:
-                    if deep_disease == onto_disease:
-                        output_disease.append(deep_disease)
+                for onto_disease in ontology_satisfying_diseases:
+                    print(deep_disease)
+                    print(onto_disease)
+                    if disease_mapping[deep_disease] == onto_disease:
+                        output_disease = deep_disease
+                        found = True
+                        break
+                if found:
+                    break
 
     else:
         print("Failed to fetch ontology information")
 
     return {
-        "disease": predicted_class,
-        "confidence": float(confidence),
-        "Ontology_Disease": ontology_satisfying_diseases,
-        "output_disease": output_disease
+        "disease": output_disease,
     }
 
 
@@ -162,9 +161,23 @@ async def predict(
 ):
     global EXTRA_SYMPTOMS
     EXTRA_SYMPTOMS = extra_symptoms.model_dump()
-    #query = build_sparql_query(EXTRA_SYMPTOMS)
     return{
         "Extra symptoms received"
+    }
+
+
+@app.post("/predict")
+async def predict_image(
+    file: UploadFile = File(...),
+):
+    image = await file.read()
+    image = Image.open(BytesIO(image))
+    resized_image = tf.image.resize(np.array(image), (224, 224))
+    img_batch = np.expand_dims(resized_image, 0)
+    predictions = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    return{
+        "disease": predicted_class,
     }
 
 
