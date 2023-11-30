b9d2603867c0a0a1207b2e9e9a799287dabc2d6e
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2023-11-30 19:22:20 +0530

    adding authentication apis

--- main.py
+++ main.py
@@ -7,9 +7,26 @@ import tensorflow as tf
 from pydantic import BaseModel
 from rdflib import Graph, Namespace
 import requests
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Dict, Any
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from typing_extensions import Annotated
+from typing import Union
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 MODEL = tf.keras.models.load_model("C:\\Users\\USER\\Desktop\\vgg model")
 CLASS_NAMES = ['Tomato_Bacterial_spot',
@@ -46,16 +63,16 @@ disease_mapping = {
 class ExtraSymptomModelOut(BaseModel):
     hasLeafSymptom: str = 'lesions'
     hasLeafSymptomColour: str = 'yellow'
-    #hasFruitSymptom: str = 'Spot_symptom'
-    # hasFruitSymptomColor: str = None
-    # hasStemSymptom: str = None
-    # hasStemSymptomColor: str = None
-    # hasLeafHalo: str = None
-    # hasLeafHaloColor: str = None
-    # hasFruitHalo: str = None
-    # hasFruitHaloColor: str = None
-    # hasFungusSymptom: str = None
-    # hasFungusSymptomColor: str = None
+    hasFruitSymptom: str = 'Spot_symptom'
+    hasFruitSymptomColor: str = None
+    hasStemSymptom: str = None
+    hasStemSymptomColor: str = None
+    hasLeafHalo: str = None
+    hasLeafHaloColor: str = None
+    hasFruitHalo: str = None
+    hasFruitHaloColor: str = None
+    hasFungusSymptom: str = None
+    hasFungusSymptomColor: str = None
 
 
 def build_sparql_query(symptoms):
@@ -89,6 +106,7 @@ def read_file_as_image(data, target_size=(224, 224)) -> np.ndarray:
 async def predict(
     file: UploadFile = File(...),
 ):
+
     image = await file.read()
     image = Image.open(BytesIO(image))
     resized_image = tf.image.resize(np.array(image), (224, 224))
@@ -155,15 +173,55 @@ async def predict(
     }
 
 
+@app.get("/ontology_detection")
+async def predict_ontology():
+
+    github_raw_uri = "https://raw.githubusercontent.com/mtbstn24/OntoML/main/OntoMLv3.rdf"
+
+    # Fetch the RDF file from the GitHub repository
+    response = requests.get(github_raw_uri)
+
+    if response.status_code == 200:
+        # Create a Graph
+        g = Graph()
+        g.parse(data=response.text, format="application/rdf+xml")
+
+        query = build_sparql_query(EXTRA_SYMPTOMS)
+
+        # Execute the SPARQL query
+        results = g.query(query)
+
+        ontology_satisfying_diseases = []
+
+        for row in results:
+            disease = row.diseaseName
+            ontology_satisfying_diseases.append(disease.value)
+
+        print(ontology_satisfying_diseases)
+
+    else:
+        print("Failed to fetch ontology information")
+
+    return {
+        "disease": ontology_satisfying_diseases,
+    }
+
+
 @app.post("/extra_symptoms")
 async def predict(
-    extra_symptoms: ExtraSymptomModelOut = Body(...)
+    payload: Dict[Any, Any]
 ):
     global EXTRA_SYMPTOMS
-    EXTRA_SYMPTOMS = extra_symptoms.model_dump()
+    EXTRA_SYMPTOMS = payload
     return{
-        "Extra symptoms received"
+        "message": "Data received successfully"
     }
+
+
+@app.post("/extra_symptomss")
+async def predictttt(payload: Dict[Any, Any]):
+    print(payload)
+    return {"message": "Data received successfully"}
 
 
 @app.post("/predict")
@@ -179,6 +237,148 @@ async def predict_image(
     return{
         "disease": predicted_class,
     }
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host='localhost', port=8000)
+
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(
+        fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
 
 
 if __name__ == "__main__":
